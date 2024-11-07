37e1e220e43a6bb6c6e139cf527a56b4e9cd81c7
LUYI <32973677+MRUIL@users.noreply.github.com>
2024-11-07 23:20:16 +0000

    Update video_classification.py

--- models/video_classification.py
+++ models/video_classification.py
@@ -1,11 +1,28 @@
 import torch.nn as nn
 import timm
+def process_each_frame(model, video, *args, **kwargs):
+    """
+    Pass in each frame separately
+    Args:
+        video (B, C, T, H, W)
+    Returns:
+        feats: (B, C', T, 1, 1)
+    """
+    batch_size = video.size(0)
+    time_dim = video.size(2)
+    video_flat = video.transpose(1, 2).flatten(0, 1)
+    feats_flat = model(video_flat, *args, **kwargs)
+    return feats_flat.view((batch_size, time_dim) +
+                           feats_flat.shape[1:]).transpose(
+                               1, 2).unsqueeze(-1).unsqueeze(-1)
 class FrameLevelModel(nn.Module):
     """Runs a frame level model on all the frames."""
     def __init__(self, num_classes: int, model: nn.Module = None):
         del num_classes
         super().__init__()
         self.model = model
+    def forward(self, video, *args, **kwargs):
+        return process_each_frame(self.model, video, *args, **kwargs)
 class TIMMModel(FrameLevelModel):
     def __init__(self,
                  num_classes,
